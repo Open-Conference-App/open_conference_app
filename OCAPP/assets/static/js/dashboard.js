29f8889d9ccde5e3efb3a461d666916c83bdff17
Mitchell LaBauve <malabauve@gmail.com>
2016-10-11 13:20:30 -0600

    added besic functionality of each nav item;have yet to refactor and comment code

--- OCAPP/assets/static/js/dashboard.js
+++ OCAPP/assets/static/js/dashboard.js
@@ -2,27 +2,75 @@
 
 $(document).ready(function(){
 
+  //**** slideToggles menu options ****	
+	$('.conferences').mouseenter(function(){
+        $('.sub_conferences').slideDown('medium');
+    }), $('.conferences').mouseleave(function(){
+        $('.sub_conferences').slideUp('medium');
+	});
 
-	//****toggles the display for menu items****
+	$('.account').mouseenter(function(){
+        $('.sub_account').slideDown('medium');
+  	}), $('.account').mouseleave(function(){
+        $('.sub_account').slideUp('medium');
+	});
 
-	$('.presentations, .account, .conferences').css('cursor', 'pointer')
+	$('.presentations, .account, .conferences, .contact, .institution, .conference').css('cursor', 'pointer')
+	
 
+  //**** toggles the display for menu items ****
 	$('.presentations').click(function(){
 		$('.home').css('display', 'none');
 		$('.presentations_info').css('display', 'block');
 		$('.account_info').css('display', 'none');
 	})
-	$('.account').click(function(){
-		$('.home').css('display', 'none');
-		$('.account_info').css('display', 'block');
-		$('.presentations_info').css('display', 'none');
-	})
+	// $('.account').click(function(){
+	// 	$('.home').css('display', 'none');
+	// 	$('.account_info').css('display', 'block');
+	// 	$('.presentations_info').css('display', 'none');
+	// })
 
-	 $('.first').click(function(){
+	 $('.current_conference').click(function(){
 	  	$('.home').css('display', 'block');
 		$('.account_info').css('display', 'none');
 		$('.presentations_info').css('display', 'none');
-	  })
+	})
+	$('.edit').click(function(){
+	  	$('.account_info').css('display', 'block');
+	  	$('.contact_info').css('display', 'block');
+	  	$('.membership').css('display', 'none');
+	  	$('.conference_info').css('display', 'none');
+	  	$('.institution_info').css('display', 'none');
+		$('.home').css('display', 'none');
+		$('.presentations_info').css('display', 'none');
+	})
+
+	$('.payment').click(function(){
+	  	$('.membership').css('display', 'block');
+	  	$('.conference_info').css('display', 'none');
+	  	$('.contact_info').css('display', 'none');
+	  	$('.institution_info').css('display', 'none');
+	  	$('.account_info').css('display', 'block');
+		$('.home').css('display', 'none');
+		$('.presentations_info').css('display', 'none');
+	})	
+  
+  //**** profile items toggle display ****
+  	$('.contact').click(function(){
+	  	$('.contact_info').css('display', 'block');
+		$('.institution_info').css('display', 'none');
+		$('.conference_info').css('display', 'none');
+	})
+	$('.institution').click(function(){
+	  	$('.contact_info').css('display', 'none');
+		$('.institution_info').css('display', 'block');
+		$('.conference_info').css('display', 'none');
+	})
+	$('.conference').click(function(){
+	  	$('.contact_info').css('display', 'none');
+		$('.institution_info').css('display', 'none');
+		$('.conference_info').css('display', 'block');
+	})
 
 	 // var test = $('.form').serializeArray()
 	 // console.log(test)
@@ -48,18 +96,5 @@ $(document).ready(function(){
 	 // })
 
 
-	//****slideToggles menu options****	
-
-		$('.conferences').mouseenter(function(){
-	        $('.sub_conferences').slideDown('medium');
-	  }), $('.conferences').mouseleave(function(){
-	        $('.sub_conferences').slideUp('medium');
-		  });
-
-		$('.account').mouseenter(function(){
-	        $('.sub_account').slideDown('medium');
-	  }), $('.account').mouseleave(function(){
-	        $('.sub_account').slideUp('medium');
-		  });
-
+	
 })
